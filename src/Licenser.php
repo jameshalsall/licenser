5c245cc39921a3b920bbbd9f1054ceb2c63b949e
James Halsall <james.t.halsall@googlemail.com>
2016-05-13 16:51:39 +0100

    Stripping blank lines appended at the end of a license header, adding license to source files

--- src/Licenser.php
+++ src/Licenser.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 namespace JamesHalsall\Licenser;
 
 use JamesHalsall\Licenser\Model\TokenRemoval;
@@ -283,6 +307,14 @@ class Licenser
     private function getLicenseAsComment()
     {
         $license = explode(PHP_EOL, $this->licenseHeader);
+
+        // if there are a bunch of new lines at the end of the license file
+        // then we want to remove these
+        while (end($license) === '') {
+            array_pop($license);
+        }
+        reset($license);
+
         $license = array_map(function ($licenseLine) {
             return rtrim(' * ' . $licenseLine);
         }, $license);
